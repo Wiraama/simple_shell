9af6cfe913d74f36f0cf5f6c62f2c5bc36c2cb55
Wiraama <mawira01@students.uonbi.ac.ke>
2024-02-29 15:19:17 -0800

    simpleshell

--- shell.c
+++ shell.c
@@ -1,74 +1,180 @@
 #include "shell.h"
 
-
+/**
+ * main - the main function
+ *
+ * Return: (Success) 0 always
+ * ------- (Fail) we drop out the looser :)
+ */
 int main(void)
 {
-	char command[MAX_COMMAND_LENGTH];
-	char **args;
-	char cwd[1024];
+	sh_t data;
+	int pl;
 
-	printf("$ ");
-	fflush(stdout); /** keeps accepting the command without exiting **/
-
-	while(1)
+	_memset((void *)&data, 0, sizeof(data));
+	signal(SIGINT, signal_handler);
+	while (1)
 	{
-		/** geting end of file **/
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+		index_cmd(&data);
+		if (read_line(&data) < 0)
 		{
-			printf("\n");
+			if (isatty(STDIN_FILENO))
+				PRINT("\n");
 			break;
 		}
-
-		/** removing white space **/
-		command[strcspn(command, "\n")] = '\0';
-	
-		/** parsing command to argyument **/
-		args = token_command(command);
-
-		if (args[0] != NULL)
+		if (split_line(&data) < 0)
 		{
-			if (strcmp(args[0], "exit") == 0)
-			{
-				printf("Exitting...\n");
-				sleep(2);
-				break;
-			}
-			else if (strcmp(args[0], "cd") == 0)
-			{
-				if (args[1] == NULL)
-				{
-					printf("Error: no directory provided\n");
-				}
-
-				else
-				{
-					if (chdir(args[1]) == 0)
-					{
-						/** directory changed **/
-						if (getcwd(cwd, sizeof(cwd)) != NULL)
-						{
-							printf("/%s ", cwd);
-							fflush(stdout);
-						}
-						else {
-							perror("getcwd");
-						}
-					}
-					else
-					{
-						perror("chdir");
-					}
-				}
-			}
-			else
-			{
-				command_execute(args);
-			}
+			free_data(&data);
+			continue;
 		}
-		/** display prompt again **/
-		printf("$ ");
-		fflush(stdout);
+		pl = parse_line(&data);
+		if (pl == 0)
+		{
+			free_data(&data);
+			continue;
+		}
+		if (pl < 0)
+		{
+			print_error(&data);
+			continue;
+		}
+		if (process_cmd(&data) < 0)
+		{
+			print_error(&data);
+			break;
+		}
+		free_data(&data);
 	}
+	free_data(&data);
+	exit(EXIT_SUCCESS);
+}
 
+/**
+ * read_line - read a line from the standard input
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int read_line(sh_t *data)
+{
+	char *csr_ptr, *end_ptr, c;
+	size_t size = BUFSIZE, read_st, length, new_size;
+
+	data->line = malloc(size * sizeof(char));
+	if (data->line == NULL)
+		return (FAIL);
+	if (isatty(STDIN_FILENO))
+		PRINT(PROMPT);
+	for (csr_ptr = data->line, end_ptr = data->line + size;;)
+	{
+		read_st = read(STDIN_FILENO, &c, 1);
+		if (read_st == 0)
+			return (FAIL);
+		*csr_ptr++ = c;
+		if (c == '\n')
+		{
+			*csr_ptr = '\0';
+			return (SUCCESS);
+		}
+		if (csr_ptr + 2 >= end_ptr)
+		{
+			new_size = size * 2;
+			length = csr_ptr - data->line;
+			data->line = _realloc(data->line, size * sizeof(char),
+						new_size * sizeof(char));
+			if (data->line == NULL)
+				return (FAIL);
+			size = new_size;
+			end_ptr = data->line + size;
+			csr_ptr = data->line + length;
+		}
+	}
+}
+#define DELIMITER " \n\t\r\a\v"
+/**
+ * split_line - splits line to tokens
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int split_line(sh_t *data)
+{
+	char *token;
+	size_t size = TOKENSIZE, new_size, i = 0;
+
+	if (_strcmp(data->line, "\n") == 0)
+		return (FAIL);
+	data->args = malloc(size * sizeof(char *));
+	if (data->args == NULL)
+		return (FAIL);
+	token = strtok(data->line, DELIMITER);
+	if (token == NULL)
+		return (FAIL);
+	while (token)
+	{
+		data->args[i++] =  token;
+		if (i + 2 >= size)
+		{
+			new_size = size * 2;
+			data->args = _realloc(data->args, size * sizeof(char *),
+					new_size * sizeof(char *));
+			if (data->args == NULL)
+				return (FAIL);
+			size = new_size;
+		}
+		token = strtok(NULL, DELIMITER);
+	}
+	data->args[i] = NULL;
+	return (0);
+}
+#undef DELIMITER
+#define DELIMITER ":"
+/**
+ * parse_line - parses arguments to valid command
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int parse_line(sh_t *data)
+{
+	if (is_path_form(data) > 0)
+		return (SUCCESS);
+	if (is_builtin(data) > 0)
+	{
+		if (handle_builtin(data) < 0)
+			return (FAIL);
+		return (NEUTRAL);
+	}
+	is_short_form(data);
+	return (SUCCESS);
+}
+#undef DELIMITER
+/**
+ * process_cmd - process command and execute process
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int process_cmd(sh_t *data)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		if (execve(data->cmd, data->args, environ) < 0)
+		data->error_msg = _strdup("not found\n");
+			return (FAIL);
+	}
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
 	return (0);
 }
